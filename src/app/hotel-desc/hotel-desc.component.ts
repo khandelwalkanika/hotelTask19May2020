47528b610bed363abd0c8391d0ca283d54d417e0
khandelwalkanika <khandelwalkanika@github.com>
2020-05-19 23:59:03 +0530

    added time logic

--- src/app/hotel-desc/hotel-desc.component.ts
+++ src/app/hotel-desc/hotel-desc.component.ts
@@ -20,17 +20,17 @@ const DaywisePrices = {
   styleUrls: ['./hotel-desc.component.css']
 })
 export class HotelDescComponent implements OnInit {
- id :number;
- hotelData : any;
+  id: number;
+  hotelData: any;
   hotels = Hotels
   hotel: Hotel;
-  
+
   // hotel = this.hotels[1].name;
   price: number = 0;
   constructor(private route: ActivatedRoute,
     private router: Router) { }
 
-  
+
 
   ngOnInit(): void {
     this.route.params
@@ -41,8 +41,8 @@ export class HotelDescComponent implements OnInit {
         }
       );
 
-      this.route.data.subscribe(data => {
-        this.hotelData=data;
+    this.route.data.subscribe(data => {
+      this.hotelData = data;
     })
   }
 
@@ -54,12 +54,19 @@ export class HotelDescComponent implements OnInit {
     const numRooms = Number(value.roomNum);
     const startDay = new Date(value.fromdate).getDay();
     const endDay = new Date(value.todate).getDay();
+    const checkoutTime = Number(value.checkoutTime.replace(':', ''));
+
     this.price = 0
     for (let i = 0; i < numOfDays; i++) {
       this.price += DaywisePrices[new Date(new Date(value.fromdate).getTime() + 24 * 3600 * 1000 * i).getDay()]
     }
+    // if he checkout post 11 am !!! 
+    if (checkoutTime > 1100) {
+      this.price += DaywisePrices[endDay]
+      
+    }
     this.price = this.price * numRooms;
     console.log("price", this.price)
-    this.router.navigate(['/invoice'], {state:{data : {...value, price: this.price}}})
+    this.router.navigate(['/invoice'], { state: { data: { ...value, price: this.price } } })
   }
 }
